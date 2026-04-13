4e5f3e320dc862ca6d71580ddee09d1e3e0a87e8
Maxim Lezhankov <lemmaksim44@gmail.com>
2026-04-13 18:01:12 +0300

    fix: Переработан pipeline

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,7 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_KEY')
+env = environ.Env()
+env_file = BASE_DIR / '.env'
+environ.Env.read_env(str(env_file))
+
+SECRET_KEY = env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
